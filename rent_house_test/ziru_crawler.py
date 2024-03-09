9475b6170d861233541bf85c2155ac087b38910d
金栩 <623984178@qq.com>
2024-03-09 21:31:26 +0800

    1.1自如： 已完成： 1.测试获取租房具体信息 2.价格获取 需解决： 房屋标签如何存储到数据库中

--- rent_house_test/ziru_crawler.py
+++ rent_house_test/ziru_crawler.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import re
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 SLBrowser/9.0.3.1311 SLBChan/25'
@@ -21,6 +22,78 @@ new_url = 'https:' + house
 house_htm = requests.get(new_url, headers=headers).content.decode('UTF-8')
 house_html = etree.HTML(house_htm)
 
+def switch_red(str):
+    if str == "0":
+        return "6"
+    elif str == "30":
+        return "1"
+    elif str == "60":
+        return "9"
+    elif str == "90":
+        return "7"
+    elif str == "120":
+        return "4"
+    elif str == "150":
+        return "5"
+    elif str == "180":
+        return "0"
+    elif str == "210":
+        return "8"
+    elif str == "240":
+        return "3"
+    elif str == "270":
+        return "2"
+
+
+def switch(str):
+    if str == "0":
+        return "7"
+    elif str == "30":
+        return "2"
+    elif str == "60":
+        return "6"
+    elif str == "90":
+        return "3"
+    elif str == "120":
+        return "8"
+    elif str == "150":
+        return "4"
+    elif str == "180":
+        return "0"
+    elif str == "210":
+        return "1"
+    elif str == "240":
+        return "9"
+    elif str == "270":
+        return "5"
+
+
+def get_price(html):
+    price_html = html.xpath('//div[@class="Z_price"]')
+    # print(price_html[0].xpath('./span/@class')[0])
+    price_h = price_html[0].xpath('.//i[@class="num"]/@style')
+    # print(price_html[0].xpath('.//i[@class="num"]/@style'))
+    price = "￥"
+    # price_math = []
+    if price_html[0].xpath('./span/@class')[0] == "red":
+        for i in price_h:
+            price = price + switch_red(re.findall('background-position:-(.*?)px;', i)[0])
+            # price_math.append(re.findall('background-position:-(.*?)px;', i)[0])
+            # price_math.append(switch_red(re.findall('background-position:-(.*?)px;', i)[0]))
+        # print(re.findall('background-position:(.*?);', price_h[0]))
+        # print(price_math)
+    else:
+        for i in price_h:
+            price = price + switch(re.findall('background-position:-(.*?)px;', i)[0])
+            # price_math.append(re.findall('background-position:-(.*?)px;', i)[0])
+            # price_math.append(switch(re.findall('background-position:-(.*?)px;', i)[0]))
+        # print(re.findall('background-position:(.*?);', price_h[0]))
+    # print(price)
+    return price
+
+
+price = get_price(house_html)
+# print(price)
 name = house_html.xpath('/html/body/div[1]/section/aside/h1/text()')[0]
 # print(name)
 house_info = house_html.xpath('//div[@class="Z_home_b clearfix"]/dl/dd/text()')
@@ -35,7 +108,18 @@ time = house_html.xpath('//*[@id="live-tempbox"]/ul/li[2]/span[@class="info_valu
 # print(time)
 # print(live_time)
 house_tag = house_html.xpath('/html/body/div[1]/section/aside/div[2]//span/text()')
-print(house_tag)
+# print(house_tag)
 
+
+# price_html = house_html.xpath('//div[@class="Z_price"]')
+# print(price_html[0].xpath('./span/@class'))
+# price_h = price_html[0].xpath('.//i[@class="num"]/@style')
+# print(price_html[0].xpath('.//i[@class="num"]/@style'))
+#
+# price_math = []
+# for i in price_h:
+#     price_math.append(re.findall('background-position:-(.*?)px;', i)[0])
+# # print(re.findall('background-position:(.*?);', price_h[0]))
+# print(price_math)
 
 
