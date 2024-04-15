6a221cb3e0d3cc362036ff48216f0c8048d4fd5b
金栩 <623984178@qq.com>
2024-04-15 13:37:36 +0800

    2.9后端API增加收藏功能的接口

--- rent_house_test/rent_api.py
+++ rent_house_test/rent_api.py
@@ -9,6 +9,8 @@ APPID = 'wxe7134cbc391a01e4'
 SECRET = 'e8973b95c8d3d4674c64e2d9dc1189e9'
 
 
+
+
 @app.route('/login', methods=['POST'])
 def login():
     code = request.json.get('code')
@@ -35,24 +37,109 @@ def login():
         print(data)
         return jsonify({'error': 'Failed to get openid and session_key'}), 500
 
-
 # 连接数据库
 db = pymysql.connect(host='localhost', user='root', password='root', db='rent_house')
 cursor = db.cursor()
-
-
 @app.route('/getData', methods=['GET'])
 def getData():
-    sql = "SELECT name, place, price, href, tag, square, img_src ,source FROM house_info"
+    sql = "SELECT name, place, price, href, tag, square, img_src, source, id FROM house_info"
     try:
         cursor.execute(sql)
         result = cursor.fetchall()
-        items_dict = [
-            {'name': item[0], 'place': item[1], 'price': item[2], 'href': item[3], 'tag': item[4], 'square': item[5],
-             'img_src': item[6], 'source': item[7]} for item in result]
-        return jsonify(items_dict)
+        if result is not None:
+            items_dict = [
+                {'name': item[0], 'place': item[1], 'price': item[2], 'href': item[3], 'tag': item[4],
+                 'square': item[5],
+                 'img_src': item[6], 'source': item[7], 'houseId': item[8]} for item in result]
+            return jsonify(items_dict)
+    except Exception as e:
+        print(f"An error occurred in getData: {e}")
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/getStoreData', methods=['POST'])
+def getStoreData():
+    openId = request.json.get('openid')
+    # print(openId)
+    if not openId:
+        return jsonify({'error': 'Missing openId'}), 400
+    sql = ('SELECT house_info.name, house_info.price, house_info.square, house_info.place, house_info.tag, '
+           'house_info.href, house_info.img_src, house_info.source, house_info.id FROM house_info,store_info WHERE userId = %s AND '
+           'house_info.`id` = store_info.`houseId`')
+    try:
+        cursor.execute(sql, openId)
+        # db.commit()
+        result = cursor.fetchall()
+        if result is not None:
+            items_dict = [
+                {'name': item[0], 'place': item[3], 'price': item[1], 'href': item[5], 'tag': item[4],
+                 'square': item[2],
+                 'img_src': item[6], 'source': item[7], 'houseId': item[8]} for item in result]
+            # print(items_dict)
+            return jsonify(items_dict)
     except Exception as e:
         print(f"An error occurred: {e}")
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/deleteStore', methods=['POST'])
+def deleteStore():
+    openId = request.json.get('openid')
+    houseId = request.json.get('houseId')
+    print(openId)
+    print(houseId)
+    if not openId:
+        return jsonify({'error': 'Missing openId'}), 400
+    sql = 'DELETE FROM store_info WHERE userId = %s AND houseId = %s'
+    try:
+        cursor.execute(sql, (openId, houseId))
+        db.commit()
+        return 'success: delete'
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/insertStore', methods=['POST'])
+def insertData():
+    openId = request.json.get('openid')
+    houseId = request.json.get('houseId')
+    # print(openId)
+    # print(houseId)
+    if not openId:
+        return jsonify({'error': 'Missing openId'}), 400
+    sql = 'INSERT INTO store_info VALUES(%s, %s)'
+    try:
+        cursor.execute(sql, (openId, houseId))
+        db.commit()
+        return 'success: insert'
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/judgeStore', methods=['POST'])
+def judgeStore():
+    openId = request.json.get('openid')
+    print(openId)
+    if not openId:
+        return jsonify({'error': 'Missing openId'}), 400
+    sql = 'SELECT houseId FROM store_info WHERE userId=%s'
+    try:
+        cursor.execute(sql, openId)
+        result = cursor.fetchall()
+        if result is None:
+            return "None"
+        else:
+            favoriteMap = {}
+            for item in result:
+                # print(item[0])
+                favoriteMap[item[0]] = True
+            print(favoriteMap)
+            return jsonify(favoriteMap)
+
+    except Exception as e:
+        print(f"An error occurred in judgeStore: {e}")
         return jsonify({'error': str(e)}), 500
 
 
--- rent_house_test/test.py
+++ rent_house_test/test.py
@@ -1,2 +1,23 @@
-s = "12.3㎡ | 31/33层 | 朝南"
-print(s.replace(" ", "").split('|'))
+# s = "12.3㎡ | 31/33层 | 朝南"
+# print(s.replace(" ", "").split('|'))
+import pymysql
+from flask import jsonify
+
+# 连接数据库
+db = pymysql.connect(host='localhost', user='root', password='root', db='rent_house')
+cursor = db.cursor()
+sql = 'SELECT houseId FROM store_info WHERE userId=%s'
+openId = 'oWoWf5AO2X81wOjaQehQtSEK3Nr4'
+try:
+    cursor.execute(sql, openId)
+    result = cursor.fetchall()
+    favoriteMap = {}
+    for item in result[:-1]:
+        favoriteMap[item[0]] = True
+    print(favoriteMap)
+    print(type(favoriteMap))
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    print({'error': str(e)}, 500)
+
